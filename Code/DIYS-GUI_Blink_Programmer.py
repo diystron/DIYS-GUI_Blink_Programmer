95ba4c038af0470175a6feb56ba4da385c730785
bagasarya93 <bagbagarya@gmail.com>
2023-03-16 21:51:14 +0700

    update

--- Code/DIYS-GUI_Blink_Programmer.py
+++ Code/DIYS-GUI_Blink_Programmer.py
@@ -1,1 +1,10 @@
+from tkinter import *
+import serial.tools.list_ports
 
+
+root =Tk()
+root.title ("DIYS - Blink Programmer")
+root.geometry("500x500")
+root.config(background="white")
+
+root.maimloop()
