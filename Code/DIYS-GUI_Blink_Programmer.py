552d355a1f0591bf48304b3f7eedaac0df5c9e39
bagasarya93 <bagbagarya@gmail.com>
2023-03-25 15:41:48 +0700

    update

--- Code/DIYS-GUI_Blink_Programmer.py
+++ Code/DIYS-GUI_Blink_Programmer.py
@@ -1,8 +1,9 @@
 from tkinter import *
 import serial.tools.list_ports
 
+
 def connect_init():
-    global root, connect_btn
+    global root, connect_btn, refresh_btn, ser
     root = Tk()
     root.title("DIYS - Blink Programmer")
     root.geometry("500x500") #px
@@ -21,18 +22,20 @@ def connect_init():
     refresh_btn.grid(column=3, row=1, pady=10, padx=10)
 
     #show "connect" button
-    connect_btn= Button(root, text="connect", height=1, width=7, state="disable") #?
+    connect_btn= Button(root, text="connect", height=1, width=7, state="disable", command=connexion) #?
     connect_btn.grid(column=3, row=2, pady=10, padx=10)
 
     update_com()
     baud_select()
+    content()
+    
 
 def connect_ceck(args):
     if "-" in clicked_com.get() or "-" in clicked_bd.get():
-        connect_btn["state"]="disable"
+        connect_btn["state"] = "disable"
 
     else :
-        connect_btn["state"]="active"
+        connect_btn["state"] = "active"
 
 def baud_select():
     global clicked_bd, drob_bd
@@ -56,7 +59,7 @@ def baud_select():
     ]
 
     clicked_bd.set(bds[0])
-    drob_bd=OptionMenu(root, clicked_bd, *bds) #?
+    drob_bd=OptionMenu(root, clicked_bd, *bds, command=connect_ceck) #?
     drob_bd.config(width=20)
     drob_bd.grid(column=2, row=2, padx=50)
 
@@ -79,8 +82,52 @@ def update_com():
     drob_COM.grid(column=2, row=1, padx=50)
     connect_ceck(0)
 
-#def connexion():
-#    if connect_btn=
+def connexion():
+    global connect_btn, serialData, ser
+    if connect_btn["text"] in "Disconnect:":
+        serialData = False
+        connect_btn["text"] = "Connect"
+        refresh_btn["state"] = "active"
+        drob_bd["state"] = "active"
+        drob_COM["state"] = "active"
+        serialData = False
+
+    else:
+        serialData = True
+        connect_btn["text"] = "Disconnect:"
+        refresh_btn["state"] = "disable"
+        drob_bd["state"] = "disable"
+        drob_COM["state"] = "disable"
+        port = clicked_com.get()
+        baud = clicked_bd.get()
+
+        try:
+            ser = serial.Serial(port, baud, timeout=0)
+
+        except:
+            pass
+
+def dataSend():
+    global entryTimeHigh, ser
+    ser.write(b'b')
+    
+
+def content():
+    global entryTimeHigh
+    label_BlinkSetting = Label(root, text="BLINK SETTING", background="white")
+    label_BlinkSetting.grid(column=2, row=5, pady=10, padx=10)
+
+    label_TimeHigh = Label(root, text="WAKTU NYALA", background="white")
+    label_TimeHigh.grid(column=1, row=7, padx=10, pady=2)
+
+    entryTimeHigh = Entry(root, width=5)
+    entryTimeHigh.insert(0, "500")
+    entryTimeHigh.grid(column=2, row=7)
+
+    save_btn = Button(root, text="SAVE", height=1, width=4, command=dataSend)
+    save_btn.grid(column=3, row=7)
+
+
 
 
     
